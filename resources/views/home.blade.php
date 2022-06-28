8d7769ff17d08b9e30f7c4d535b662318a7baadd
Md. Shohan Hossain Ean <hossain_shohan1@live.com>
2022-06-28 15:42:41 +0600

    table responsive issue solved

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -61,68 +61,70 @@
                             {{ session('success') }}
                         </div>
                     @endif
-                    <table class="table table-bordered table-hover text-center" id="mainTable">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">Phone Number</th>
-                                <th scope="col">Interested In</th>
-                                <th scope="col">Code</th>
-                                <th scope="col">Used Status</th>
-                                <th scope="col">Discount</th>
-                                <th scope="col">Code Generated At</th>
-                                @if (auth()->id() != 4)
-                                    <th scope="col">Action</th>
-                                @endif
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($wheels as $wheel)
+                    <div class="table-responsive">
+                        <table class="table table-bordered table-hover text-center" id="mainTable">
+                            <thead>
                                 <tr>
-                                    <th scope="row" class="text-center">{{ $loop->index+1 }}</th>
-                                    <td><i class="lni lni-phone"></i> {{ $wheel->phone_number }}</td>
-                                    <td>
-                                        <i class="{{ ($wheel->course_type == 'online') ? 'lni lni-headphone-alt':'lni lni-map' }}"></i>
-                                        {{ $wheel->course_type }}
-                                    </td>
-                                    <td>
-                                        <script>
-                                            function copytoclipboard(e) {
-                                                var copyText = e.value;
-                                                navigator.clipboard.writeText(copyText);
-                                            }
-                                        </script>
-                                        {{ $wheel->code }}
-                                        <button value="{{ $wheel->code }}" onclick="copytoclipboard(this)" class="btn btn-outline-primary"><i class="lni lni-files"></i></button>
-                                    </td>
-                                    <td>
-                                        @if ($wheel->used_status)
-                                            <span class="badge bg-success">Used</span>
-                                        @else
-                                            <span class="badge bg-danger">Not Used Yet</span>
-                                        @endif
-                                    </td>
-                                    <td>{{ $wheel->discount ?? '-' }}</td>
-                                    <td>
-                                        <div class="badge bg-info text-dark">
-                                            {{ $wheel->created_at->timezone('Asia/Dhaka')->diffforhumans() }}
-                                        </div>
-                                        <br>
-                                        Date: {{ $wheel->created_at->timezone('Asia/Dhaka')->format('d/m/Y') }}
-                                        <br>
-                                        Time: {{ $wheel->created_at->timezone('Asia/Dhaka')->format('H:i:s A') }}
-                                    </td>
+                                    <th scope="col">#</th>
+                                    <th scope="col">Phone Number</th>
+                                    <th scope="col">Interested In</th>
+                                    <th scope="col">Code</th>
+                                    <th scope="col">Used Status</th>
+                                    <th scope="col">Discount</th>
+                                    <th scope="col">Code Generated At</th>
                                     @if (auth()->id() != 4)
-                                    <td>
-                                        @if (!$wheel->used_status)
-                                            <a href="{{ url('resend/code') }}/{{ $wheel->id }}" class="btn btn-sm btn-warning">Resend Code</a>
-                                        @endif
-                                    </td>
+                                        <th scope="col">Action</th>
                                     @endif
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($wheels as $wheel)
+                                    <tr>
+                                        <th scope="row" class="text-center">{{ $loop->index+1 }}</th>
+                                        <td><i class="lni lni-phone"></i> {{ $wheel->phone_number }}</td>
+                                        <td>
+                                            <i class="{{ ($wheel->course_type == 'online') ? 'lni lni-headphone-alt':'lni lni-map' }}"></i>
+                                            {{ $wheel->course_type }}
+                                        </td>
+                                        <td>
+                                            <script>
+                                                function copytoclipboard(e) {
+                                                    var copyText = e.value;
+                                                    navigator.clipboard.writeText(copyText);
+                                                }
+                                            </script>
+                                            {{ $wheel->code }}
+                                            <button value="{{ $wheel->code }}" onclick="copytoclipboard(this)" class="btn btn-outline-primary"><i class="lni lni-files"></i></button>
+                                        </td>
+                                        <td>
+                                            @if ($wheel->used_status)
+                                                <span class="badge bg-success">Used</span>
+                                            @else
+                                                <span class="badge bg-danger">Not Used Yet</span>
+                                            @endif
+                                        </td>
+                                        <td>{{ $wheel->discount ?? '-' }}</td>
+                                        <td>
+                                            <div class="badge bg-info text-dark">
+                                                {{ $wheel->created_at->timezone('Asia/Dhaka')->diffforhumans() }}
+                                            </div>
+                                            <br>
+                                            Date: {{ $wheel->created_at->timezone('Asia/Dhaka')->format('d/m/Y') }}
+                                            <br>
+                                            Time: {{ $wheel->created_at->timezone('Asia/Dhaka')->format('H:i:s A') }}
+                                        </td>
+                                        @if (auth()->id() != 4)
+                                        <td>
+                                            @if (!$wheel->used_status)
+                                                <a href="{{ url('resend/code') }}/{{ $wheel->id }}" class="btn btn-sm btn-warning">Resend Code</a>
+                                            @endif
+                                        </td>
+                                        @endif
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
